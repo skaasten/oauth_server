271190c7cf35f356009d0ea948576765206bff61
Shaun Kaasten <shaunk@gmail.com>
2015-02-18 11:25:17 -0500

    Add ;

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.post('/oauth/token', function (req, res) {
 
 
 var server = app.listen(3000, function () {
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 
